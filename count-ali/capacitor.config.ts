819e0907de1dd8fe683077218929ab00a5a67d05
Patricio Lagos <Pat.lagos@duocuc.cl>
2023-06-26 00:41:36 -0400

    commmit inicial

---
+++ count-ali/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'countAli',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
